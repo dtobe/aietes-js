a5f9940f09e5f9bfecd0363f73492ddbbd761135
dtobe <tobias_ehrler@yahoo.co.uk>
2019-05-10 18:04:32 +0200

    Extracted meta data into separate module

--- mock-server.js
+++ mock-server.js
@@ -3,6 +3,7 @@ const _ = require("lodash");
 const enableDestroy = require("server-destroy");
 const morgan = require("morgan");
 const unconfiguredRoutesHandler = require('./lib/errorHandler');
+const MetaData = require('./lib/metaData');
 
 const SUPPORTED_METHODS = ["get", "post", "put", "delete"];
 const isDebug = process.env.DEBUG || false;
@@ -17,13 +18,13 @@ class AietesServer {
   constructor(responses, port) {
     // clone responses object to avoid external changes to the reference
     this.responses = Object.assign({}, responses);
-    this.responsesMetaData = {};
+    this.responsesMetaData = new MetaData(this.getNumResponses);
     this.serverPort = port;
     this.app = null;
     this.server = null;
     this._setup();
   }
-
+  
   start() {
     try {
       this._listen(() => {
@@ -38,25 +39,26 @@ class AietesServer {
       process.exit(1);
     }
   }
-
+  
   update(responses) {
+    log('Updating responses');
     Object.assign(this.responses, responses);
   }
-
+  
   reset(responses) {
-    console.log("Restarting Aietes server");
+    log("Restarting Aietes server");
     this._end();
     this.responses = Object.assign({}, responses);
-    this.responsesMetaData = {};
+    this.responsesMetaData.clear();
     this._setup();
     this.start();
   }
-
+  
   stop() {
-    console.log("Exiting Aietes server");
+    log("Exiting Aietes server");
     this._end();
   }
-
+  
   _setup() {
     this.app = express();
     this.app.use(morgan("tiny"));
@@ -66,14 +68,14 @@ class AietesServer {
     this._makeRoutes();
     this.app.use(unconfiguredRoutesHandler);
   }
-
+  
   _listen(callback) {
     this.server = this.app.listen(this.serverPort, () => {
       callback();
     });
     enableDestroy(this.server);
   }
-
+  
   _end() {
     this.server.destroy();
   }
@@ -81,9 +83,9 @@ class AietesServer {
   _makeRoutes() {
     _.each(this.responses, (responsesByMethod, path) => {
       Object.keys(responsesByMethod).forEach((method) => {
-        this._initMetaDataForPath(path, method);
         const methodForExpress = method.toLowerCase();
         if (SUPPORTED_METHODS.includes(methodForExpress)) {
+          this.responsesMetaData.initMetaDataForHandler(path, method);
           this.app[methodForExpress](path, this._createHandler(path, method));
         } else {
           console.warn(`Method ${method} is not supported. Path '${path}'-${method} will be skipped.`);
@@ -92,35 +94,20 @@ class AietesServer {
     });
   }
 
-  _initMetaDataForPath(path, method) {
-    if (!this.responsesMetaData[path]) {
-      this.responsesMetaData[path] = {};
-    }
-    if (!this.responsesMetaData[path][method]) {
-      this.responsesMetaData[path][method] = {};
-    }
-    this.responsesMetaData[path][method].currentResponse = 0;
-  }
-
   _createHandler(path, method) {
     return (req, res) => {
       const endPointResponse = this.responses[path][method];
       let currentResponse;
       if (Array.isArray(endPointResponse)) {
-        const currentResponseIndex = this.responsesMetaData[path][method].currentResponse;
-        currentResponse = endPointResponse[currentResponseIndex];
-        this._nextResponse(path, method);
+        log('Getting next response index');
+        const responseIndex = this.responsesMetaData.nextResponseIndex(path, method, endPointResponse.length);
+        log(`Current ResponseIndex = ${responseIndex}`);
+        currentResponse = endPointResponse[responseIndex];
       } else {
         currentResponse = endPointResponse;
       }
       return this._sendResponse(res, currentResponse);
     }
-  }
-
-  _nextResponse(path, method) {
-    const currentValue = this.responsesMetaData[path][method].currentResponse;
-    const maxValue = this.responses[path][method].length;
-    this.responsesMetaData[path][method].currentResponse = (currentValue + 1) % maxValue;
   }
 
   _sendResponse(res, responseData) {
