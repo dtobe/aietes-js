30eb311cadbef7de72251a26e2ca7b9684f72de8
Kai Tait <kai.tait@rewe-digital.com>
2019-05-19 15:22:06 +0200

    added option to cofigure response object via cmd line args

--- standalone/index.js
+++ standalone/index.js
@@ -1,19 +1,21 @@
 const getPort = require("get-port");
+const argv = require("yargs").argv;
 const MockServer = require("../mock-server");
 
 standalone = async () => {
-    const randomPort = await getPort();
+
+    const _port = argv.port || await getPort();
     const responseObject = {
-        status: 201,
-        headers: { "some-header": "abc-123" },
-        data: { hello: "Aietes"}
+        status: argv.status || 201,
+        headers: argv.headers || { "some-header": "abc-123" },
+        data: argv.data || { hello: "Aietes"}
     };
     const endpoint = {
         "/": {
             get: responseObject
         }
     };
-    const standaloneMock = new MockServer(endpoint, randomPort);
+    const standaloneMock = new MockServer(endpoint, _port);
     standaloneMock.start();
     console.info("started standalone.");
 };
