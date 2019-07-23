4a4e9b1f1c44574b9f3da591d5cc92e0a75410f8
dtobe <tobias_ehrler@yahoo.co.uk>
2019-07-23 18:05:56 +0200

    more graceful error handling when reading file

--- standalone/index.js
+++ standalone/index.js
@@ -1,31 +1,37 @@
-const getPort = require("get-port");
-const argv = require("yargs").argv;
+const getPort = require('get-port');
+const argv = require('yargs').argv;
 const fs = require('fs');
 
-const AietesServer = require("../mock-server");
+const AietesServer = require('../mock-server');
 
-standalone = async () => {
-    console.info("Starting Aietes standalone");
+const standalone = async() => {
+  console.info('Starting Aietes standalone');
 
-    const port = argv.port || await getPort();
+  const port = argv.port || await getPort();
 
-    const responseFileName = argv.json;
-    let jsonResponse;
-    if (responseFileName) {
-        jsonResponse = JSON.parse(await fs.readFile(responseFileName, 'utf8'));
+  const responseFileName = argv.json;
+  let jsonResponse;
+  if (responseFileName) {
+    try {
+      jsonResponse = JSON.parse(fs.readFileSync(responseFileName, 'utf8'));
+    } catch (err) {
+      console.error(err.message);
+      console.error(`Reading response definition file '${responseFileName}' failed. Aietes exiting.`);
+      process.exit(1);
     }
+  }
+  const response = jsonResponse || {
+    '/': {
+      get: {
+        status: 200,
+        headers: {},
+        data: { hello: 'Aietes' }
+      }
+    }
+  };
 
-    const response = jsonResponse || {
-        "/": {
-            get: {
-                status: 200,
-                headers: {},
-                data: { hello: "Aietes"}
-            }
-        }
-    };
-    const standaloneMock = new AietesServer(response, port);
-    standaloneMock.start();
+  const standaloneMock = new AietesServer(response, port);
+  standaloneMock.start();
 };
 
 module.exports = standalone();
