ca635c9084c745f205a3ff892b6af5b1da764422
dtobe <tobias_ehrler@yahoo.co.uk>
2020-05-15 16:30:03 +0200

    Issue-11: refactored mock server to give request handlers 'memory'

--- mock-server.js
+++ mock-server.js
@@ -2,16 +2,14 @@ const express = require('express');
 const _ = require('lodash');
 const enableDestroy = require('server-destroy');
 const unconfiguredRoutesHandler = require('./lib/errorHandler');
-const MetaData = require('./lib/metaData');
+const ResponseConfig = require('./lib/response');
 const { log, accessLog } = require('./lib/logging');
 
 const SUPPORTED_METHODS = ['get', 'post', 'put', 'delete'];
 
 class AietesServer {
-  constructor(responses, port) {
-    // clone responses object to avoid external changes to the reference
-    this.responses = Object.assign({}, responses);
-    this.responsesMetaData = new MetaData();
+  constructor(responsesConfig, port) {
+    this.responses = createResponses(responsesConfig);
     this.serverPort = port;
     this.app = null;
     this.server = null;
@@ -31,16 +29,23 @@ class AietesServer {
     }
   }
 
-  update(responses) {
+  update(responsesConfig) {
     log.info('Updating responses');
-    Object.assign(this.responses, responses);
+    const newResponses = createResponses(responsesConfig);
+    newResponses.forEach((newResponse) => {
+      const existingResponse = this.responses.find((savedResponse) => {
+        return savedResponse.path === newResponse.path && savedResponse.method === newResponse.method;
+      });
+      if (existingResponse) {
+        existingResponse.update(newResponse.endPointResponse);
+      }
+    });
   }
 
-  reset(responses) {
+  reset(responsesConfig) {
     log.info('Restarting Aietes server');
     this._end();
-    this.responses = Object.assign({}, responses);
-    this.responsesMetaData.clear();
+    this.responses = createResponses(responsesConfig);
     this._setup();
     this.start();
   }
@@ -51,7 +56,16 @@ class AietesServer {
   }
 
   setDelayMs(delayMs, path, method) {
-    this.responsesMetaData.setDelayMs(delayMs, path, method);
+    if (!(path && method)) {
+      this.responses.forEach((response) => {
+        response.setDelayMs(delayMs);
+      });
+    } else {
+      const existingResponse = this.responses.find((savedResponse) => {
+        return savedResponse.path === path && savedResponse.method === method;
+      });
+      existingResponse && existingResponse.setDelayMs(delayMs);
+    }
   }
 
   _setup() {
@@ -76,78 +90,25 @@ class AietesServer {
   }
 
   _makeRoutes() {
-    _.each(this.responses, (responsesByMethod, path) => {
-      Object.keys(responsesByMethod).forEach((method) => {
-        const methodForExpress = method.toLowerCase();
-        if (SUPPORTED_METHODS.includes(methodForExpress)) {
-          this.responsesMetaData.initMetaDataForHandler(path, method);
-          this.app[methodForExpress](path, this._createHandler(path, method));
-        } else {
-          log.warn(`Method ${method} is not supported. Path '${path}'-${method} will be skipped.`);
-        }
-      });
+    this.responses.forEach((response) => {
+      this.app[response.method](response.path, response.createHandler());
     });
-  }
-
-  _createHandler(path, method) {
-    return async(req, res) => {
-      logParameters(req);
-
-      const endPointResponse = this.responses[path][method];
-      let currentResponse;
-      if (Array.isArray(endPointResponse)) {
-        const responseIndex = this.responsesMetaData.nextResponseIndex(path, method, endPointResponse.length);
-        currentResponse = endPointResponse[responseIndex];
-      } else {
-        currentResponse = endPointResponse;
-      }
-      const delayMs = this.responsesMetaData.getDelayMs(path, method);
-      return createSendResponseCallback(res, currentResponse, delayMs)();
-    };
   }
 }
 
-const logParameters = req => {
-  let logMessage = '';
-  if (req.params && Object.keys(req.params).length) {
-    logMessage += `Path variables${buildParameterList(req.params)}\n`;
-  }
-  if (req.query && Object.keys(req.query).length) {
-    logMessage += `Query parameters${buildParameterList(req.query)}`;
-  }
-  if (logMessage) {
-    logMessage = `Request to ${req.path}\n` + logMessage;
-    log.info(logMessage);
-  }
-};
-
-const buildParameterList = params => {
-  let paramList = '';
-  Object.keys(params).forEach(key => {
-    paramList += `\n- ${key}: ${params[key]}`;
+const createResponses = (responsesConfig) => {
+  return _.flatMap(responsesConfig, (responsesByMethod, path) => {
+    return _.map(responsesByMethod, (responses, method) => {
+      const methodForExpress = method.toLowerCase();
+      if (SUPPORTED_METHODS.includes(methodForExpress)) {
+        return new ResponseConfig(path, methodForExpress, responses);
+      } else {
+        log.warn(`Method ${method} is not supported. Path '${path}'-${method} will be skipped.`);
+      }
+    });
+  }).filter(response => {
+    return response !== undefined;
   });
-  return paramList;
-};
-
-const createSendResponseCallback = (handlerResponse, responseData, delayMs) => {
-  return async() => {
-    const returnStatus = responseData['status'] || 200;
-    if (delayMs) {
-      log.info(`Delaying response for ${delayMs}ms`);
-      await setTimeout(() => {
-        handlerResponse
-          .status(returnStatus)
-          .set(responseData['headers'])
-          .jsonp(responseData['data']);
-      }, delayMs);
-    } else {
-      log.info('Returning immediate response');
-      handlerResponse
-        .status(returnStatus)
-        .set(responseData['headers'])
-        .jsonp(responseData['data']);
-    }
-  };
 };
 
 module.exports = AietesServer;
